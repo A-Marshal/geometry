3783c91975961ac61f5a2322d3d508985c3b143e
Anders_Marshal@hotmail.com <Anders_Marshal@hotmail.com>
2019-09-14 10:18:46 +0200

    Ok, okokok, it's idiot, so change it

--- static/js/redrawCanvas.js
+++ static/js/redrawCanvas.js
@@ -1,22 +1,22 @@
 function drawImage(imageObj) {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-    var x = 300;
-    var y = 160;
+    var x = 640;
+    var y = 480;
 
     context.drawImage(imageObj, 0, 0, x, y);
 
-    var imageData = context.getImageData(0, 0, 640, 480);
+    var imageData = context.getImageData(0, 0, x, y);
     var data = imageData.data;
 
     for(var i = 0; i < data.length; i += 4) {
       var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
       // red
-      data[i] = brightness;
+      data[i] = brightness+10;
       // green
       data[i + 1] = brightness;
       // blue
-      data[i + 2] = brightness;
+      data[i + 2] = brightness-50;
     }
 
     // overwrite original image
