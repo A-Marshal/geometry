b8a763eefe1909fcfdca6ba800050890cf9873f0
Anders_Marshal@hotmail.com <Anders_Marshal@hotmail.com>
2019-09-19 13:50:37 +0200

    W40K Canny lines, power of copy paste  = LabView

--- static/js/inspirit-jsfeat-4c7b336/minaFeatJSFnjollerialor/setZindexes.js
+++ static/js/inspirit-jsfeat-4c7b336/minaFeatJSFnjollerialor/setZindexes.js
@@ -1,3 +1,6 @@
 function reOrderZs() {
     document.getElementById("webcam").style.zIndex = "-1";
+    //Change z-index to be on top
+    document.getElementById("imageView").style.zIndex = "2";
+    document.getElementById("imageTemp").style.zIndex = "3";
 }
